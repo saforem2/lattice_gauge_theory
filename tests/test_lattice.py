1916ea37dabbd204ccfad2c4149498679bbec320
Sam Foreman <saforem2@gmail.com>
2017-11-21 17:09:05 -0600

    added tests

---
+++ tests/test_lattice.py
@@ -0,0 +1,13 @@
+import unittest
+from lattice_gauge_theory.lattice import Lattice
+from lattice_gauge_theory.lattice_site import Site
+#  from lattice_gauge_theory.update import Update
+# TODO: Finish writing classes and IMPORT
+
+class LatticeTestCase(unittest.TestCase):
+    """ Tests for Lattice class. """
+    pass
+    #  @patch('lattice_gauge_theory.lattice.Lattice.site_with_id')
+    #  @patch('lattice_gauge_theory.lattice.Lattice.site_with_id')
+
+
